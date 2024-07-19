5d83acca4df7649ad33d4492a8d94a656ee3a879
sreeramlal <124680404+sreeramlal@users.noreply.github.com>
2024-07-19 16:26:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+origin: 'https://workspacereserva.onrender.com',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true, 
